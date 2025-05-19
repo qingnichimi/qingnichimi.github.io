4305562772918233ed1358e29feb31df3e726869
taoweishu <taoweishu2000@163.com>
2025-05-19 14:58:00 +0800

    gihub page部署配置修改

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // 如果您的仓库名是 username.github.io，则不需要 basePath
-  // 如果您的仓库名是其他名称，则需要设置 basePath
-  // basePath: '/YOUR_REPO_NAME',
   // 禁用图片优化，因为 GitHub Pages 不支持
   images: {
     unoptimized: true,
